4947485b411d554a52bcd2558d17322fb1262239
Garvit Singhal <garvitsinghal305@gmail.com>
2024-09-11 00:07:52 +0530

    fix:modal

--- components/main-sections/modal.tsx
+++ components/main-sections/modal.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import Image from 'next/image';
@@ -12,9 +12,21 @@ const AarambhModal = () => {
   const [open, setOpen] = useState(true);
 
   const onCloseModal = () => setOpen(false);
+  const [width, setWidth] = useState('90%'); // Default value for SSR
+
+  useEffect(() => {
+    const updateWidth = () => {
+      setWidth(window.innerWidth > 768 ? '350px' : '90%');
+    };
+
+    updateWidth();
+
+    window.addEventListener('resize', updateWidth);
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
 
   return (
-      <Modal open={open} onClose={onCloseModal} center styles={{modal: {width: window.innerWidth > 768 ? '350px' : '90%',backgroundColor:'transparent'}}}>
+      <Modal open={open} onClose={onCloseModal} center styles={{modal: {width: width,backgroundColor:'transparent'}}}>
         <div className='flex flex-wrap justify-center'>
             <Image src={aarambhposter} alt="Aarambh Poster" className='w-fit h-fit'/>
             <Link href="/events/aarambh24" className='text-sm md:text-lg text-white mt-3 bg-[#EE4B76] p-2 px-5 rounded-lg outline-none hover:bg-transparent hover:text-[#EE4B76] border-2 border-[#EE4B76] duration-300 ease-in-out'>Register Now</Link>
