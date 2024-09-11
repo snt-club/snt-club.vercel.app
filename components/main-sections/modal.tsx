a57fb382128f6834dddc2b4cdd46eb33408b917e
Garvit Singhal <garvitsinghal305@gmail.com>
2024-09-11 21:32:16 +0530

    fix:close icon on modal

--- components/main-sections/modal.tsx
+++ components/main-sections/modal.tsx
@@ -6,13 +6,14 @@ import Image from 'next/image';
 import Link from 'next/link'
 
 import aarambhposter from '@/assets/images/AARAMBH_24_main_JC.png';
+import close_button from '@/assets/svg/close_button.svg';
 
 
 const AarambhModal = () => {
   const [open, setOpen] = useState(true);
 
   const onCloseModal = () => setOpen(false);
-  const [width, setWidth] = useState('90%'); // Default value for SSR
+  const [width, setWidth] = useState('90%'); 
 
   useEffect(() => {
     const updateWidth = () => {
@@ -25,8 +26,9 @@ const AarambhModal = () => {
     return () => window.removeEventListener('resize', updateWidth);
   }, []);
 
+
   return (
-      <Modal open={open} onClose={onCloseModal} center styles={{modal: {width: width,backgroundColor:'transparent'}}}>
+      <Modal closeIcon={<Image src={close_button} alt='Close' width={20}/>} open={open} onClose={onCloseModal} center styles={{modal: {width: width,backgroundColor:'transparent'}}}>
         <div className='flex flex-wrap justify-center'>
             <Image src={aarambhposter} alt="Aarambh Poster" className='w-fit h-fit'/>
             <Link href="/events/aarambh24" className='text-sm md:text-lg text-white mt-3 bg-[#EE4B76] p-2 px-5 rounded-lg outline-none hover:bg-transparent hover:text-[#EE4B76] border-2 border-[#EE4B76] duration-300 ease-in-out'>Register Now</Link>
