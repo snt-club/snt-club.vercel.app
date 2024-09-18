d402d8bdf691c3796db1d8ddb07a908082a65e33
Garvit Singhal <garvitsinghal305@gmail.com>
2024-09-18 22:20:01 +0530

    fix:modal width

--- components/main-sections/modal.tsx
+++ components/main-sections/modal.tsx
@@ -30,7 +30,7 @@ const AarambhModal = () => {
   return (
       <Modal closeIcon={<Image className='hover:scale-125 duration-300' src={close_button} alt='Close' width={20} />} open={open} onClose={onCloseModal} center styles={{modal: {width: width,backgroundColor:'transparent'}}}>
         <div className='flex flex-wrap justify-center'>
-            <Image src={cbootcamp} alt="Aarambh Poster" className='w-fit h-fit'/>
+            <Image src={cbootcamp} alt="Aarambh Poster" className='w-fit h-fit md:w-3/2'/>
             <Link href="/events/cbootcamp24" className='text-sm md:text-lg text-white mt-3 bg-[#EE4B76] p-2 px-5 rounded-lg outline-none hover:bg-transparent hover:text-[#EE4B76] border-2 border-[#EE4B76] duration-300 ease-in-out'>Register Now</Link> 
         </div>
       </Modal>
