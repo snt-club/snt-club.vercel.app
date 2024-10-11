49dfed2feac4c2dddfb13ef3b3d43a4f0cbd9b96
Garvit Singhal <garvitsinghal305@gmail.com>
2024-10-11 22:38:18 +0530

    Fix:bug

--- app/events/codeformers/page.tsx
+++ app/events/codeformers/page.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import sponsorLogo1 from 'assets/images/sponsor.svg';
-
-=======
 import RegisterBtn from '@/components/registerBtn';
 
 import codeformers from '@/assets/images/codeformer.jpg';
