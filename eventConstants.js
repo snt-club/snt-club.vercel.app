c25b0ffbb93ebc0b52bbe65d97f9b15784f7c66c
Mayank Sharma <shar.mayankofficial@gmail.com>
2024-11-24 13:43:29 +0530

    Refactor: update event details by commenting out Aarambh-2024

--- eventConstants.js
+++ eventConstants.js
@@ -12,16 +12,6 @@ const eventDetails = [
     eventRegLink: '/events/',
     eventRegStatus: 'closed',
   },
-
-  
-  {
-    eventName: 'C Bootcamp',
-    eventDesc: 'Join the C Bootcamp to master C programming through flexible hybrid classes, hands-on tasks, and real-time support.',
-    eventImg: c_bootcamp_img,
-    eventStatus: 'past',
-    eventRegLink: '/events/cbootcamp24',
-    eventRegStatus: 'closed',
-  },
   {
     eventName: 'CodeFormers',
     eventDesc: 'Join us for Codeformers, a thrilling monthly coding series that transforms the C programming skills, evolve and claim interesting victory taglines every month!',
@@ -29,6 +19,14 @@ const eventDetails = [
     eventStatus: 'ongoing',
     eventRegLink: '/events/codeformers',
     eventRegStatus: 'live',
+  },
+  {
+    eventName: 'C Bootcamp',
+    eventDesc: 'Join the C Bootcamp to master C programming through flexible hybrid classes, hands-on tasks, and real-time support.',
+    eventImg: c_bootcamp_img,
+    eventStatus: 'past',
+    eventRegLink: '/events/cbootcamp24',
+    eventRegStatus: 'closed',
   },
   {
     eventName: 'Tech Talk With Alumni 2.0',
@@ -46,14 +44,14 @@ const eventDetails = [
     eventRegLink: '/events/AskSnT',
     eventRegStatus: 'closed',
   },
-  {
-    eventName: 'Aarambh-2024',
-    eventDesc: "Join us at AARAMBH for an exciting session where we'll welcome freshers and introduce them to the Science and Technology Club, showcasing innovation, insights, and opportunities!",
-    eventImg: flutterworkshop_img,
-    eventStatus: 'past',
-    eventRegLink: '/events/aarambh24',
-    eventRegStatus: 'closed',
-  },
+  // {
+  //   eventName: 'Aarambh-2024',
+  //   eventDesc: "Join us at AARAMBH for an exciting session where we'll welcome freshers and introduce them to the Science and Technology Club, showcasing innovation, insights, and opportunities!",
+  //   eventImg: flutterworkshop_img,
+  //   eventStatus: 'past',
+  //   eventRegLink: '/events/aarambh24',
+  //   eventRegStatus: 'closed',
+  // },
   // {
   //   eventName: 'Tech Talk With Alumni',
   //   eventDesc: 'Tech talk with alumni Join us for an Expert talk session, connecting students with industry pioneers for insightful tech guidance!',
