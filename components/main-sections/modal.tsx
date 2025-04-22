9e2bbabc7f621faefb9f6f2e3a31c5ff432264da
sanidhya-Git <sanidhyamehra36@gmail.com>
2025-04-22 12:01:19 +0530

    techtalkcvhanged

--- components/main-sections/modal.tsx
+++ components/main-sections/modal.tsx
@@ -1,43 +1,43 @@
-"use client"
-import React, { useState,useEffect } from 'react';
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-import Image from 'next/image';
-import Link from 'next/link'
+// "use client"
+// import React, { useState,useEffect } from 'react';
+// import 'react-responsive-modal/styles.css';
+// import { Modal } from 'react-responsive-modal';
+// import Image from 'next/image';
+// import Link from 'next/link'
 
-import dev from '@/assets/images/developer_month.webp';
-import close_button from '@/assets/svg/close_button.svg';
+// import dev from '@/assets/images/developer_month.webp';
+// import close_button from '@/assets/svg/close_button.svg';
 
 
-const AarambhModal = () => {
-  const isEventActive  = true;
-  const [open, setOpen] = useState(true);
+// const AarambhModal = () => {
+//   const isEventActive  = true;
+//   const [open, setOpen] = useState(true);
 
-  const onCloseModal = () => setOpen(false);
-  const [width, setWidth] = useState('90%'); 
+//   const onCloseModal = () => setOpen(false);
+//   const [width, setWidth] = useState('90%'); 
 
-  useEffect(() => {
-    const updateWidth = () => {
-      setWidth(window.innerWidth > 768 ? '350px' : '90%');
-    };
+//   useEffect(() => {
+//     const updateWidth = () => {
+//       setWidth(window.innerWidth > 768 ? '350px' : '90%');
+//     };
 
-    updateWidth();
+//     updateWidth();
 
-    window.addEventListener('resize', updateWidth);
-    return () => window.removeEventListener('resize', updateWidth);
-  }, []);
+//     window.addEventListener('resize', updateWidth);
+//     return () => window.removeEventListener('resize', updateWidth);
+//   }, []);
 
-  if (!isEventActive) {
-    return null;
-  }
-  return (
-      <Modal closeIcon={<Image className='hover:scale-125 duration-300' src={close_button} alt='Close' width={20} />} open={open} onClose={onCloseModal} center styles={{modal: {width: width,backgroundColor:'transparent'}}}>
-        <div className='flex flex-wrap justify-center'>
-            <Image src={dev} alt="Aarambh Poster" className='w-fit h-fit md:w-3/2 md:h-3/2'/>
-            {/* <Link href="/" className='text-sm md:text-lg text-white mt-3 bg-[#EE4B76] p-2 px-5 rounded-lg outline-none hover:bg-transparent hover:text-[#EE4B76] border-2 border-[#EE4B76] duration-300 ease-in-out'>Register Now</Link>  */}
-        </div>
-      </Modal>
-  );
-};
+//   if (!isEventActive) {
+//     return null;
+//   }
+//   return (
+//       <Modal closeIcon={<Image className='hover:scale-125 duration-300' src={close_button} alt='Close' width={20} />} open={open} onClose={onCloseModal} center styles={{modal: {width: width,backgroundColor:'transparent'}}}>
+//         <div className='flex flex-wrap justify-center'>
+//             <Image src={dev} alt="Aarambh Poster" className='w-fit h-fit md:w-3/2 md:h-3/2'/>
+//             {/* <Link href="/" className='text-sm md:text-lg text-white mt-3 bg-[#EE4B76] p-2 px-5 rounded-lg outline-none hover:bg-transparent hover:text-[#EE4B76] border-2 border-[#EE4B76] duration-300 ease-in-out'>Register Now</Link>  */}
+//         </div>
+//       </Modal>
+//   );
+// };
 
-export default AarambhModal;
+// export default AarambhModal;
