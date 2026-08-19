0569c38498c2736bc905a7b3a2c6a17819d1da0b
Viren Soni <71062471+VirenSoni29@users.noreply.github.com>
2026-08-19 09:53:19 +0530

    viren_Update email validation to include Gmail in EventRegistration model

--- models/EventRegistration.ts
+++ models/EventRegistration.ts
@@ -11,7 +11,7 @@ const EventRegistrationSchema = new Schema(
       required: true,
       lowercase: true,
       trim: true,
-      match: [/^[a-zA-Z0-9._%+-]+@skit\.ac\.in$/, "Invalid domain"],
+      match: [/^[a-zA-Z0-9._%+-]+@(gmail\.com|skit\.ac\.in)$/, "Invalid domain"],
     },
     rollNo: { type: String, required: true, trim: true },
     phone: {
